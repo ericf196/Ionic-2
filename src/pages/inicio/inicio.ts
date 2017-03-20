1c3cd7d7671ae0c272a97b4447a0d817b38f539f
Carlos Bullones <carlos.bullones@dncomputing.com>
2017-03-20 16:13:38 -0430

    part 2 tutorial ionic

--- src/pages/inicio/inicio.ts
+++ src/pages/inicio/inicio.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Geolocation } from 'ionic-native';
+import { Platform } from 'ionic-angular';
+
+declare var google: any;
+
 
 /*
   Generated class for the Inicio page.
@@ -13,7 +18,47 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class InicioPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+map: any; // Manejador del mapa.
+coords : any = { lat: 0, lng: 0 }
+
+  constructor(
+  public navCtrl: NavController,
+  public navParams: NavParams,
+  public  platform: Platform) {
+
+        platform.ready().then(() => {
+        // La plataforma esta lista y ya tenemos acceso a los plugins.
+          this.obtenerPosicion();
+       });
+
+  }
+
+
+
+  obtenerPosicion():any{
+    Geolocation.getCurrentPosition().then(res => {
+      this.coords.lat = res.coords.latitude;
+      this.coords.lng = res.coords.longitude;
+
+      this.loadMap();
+    })
+    .catch(
+      (error)=>{
+        console.log(error);
+      }
+    );
+  }
+
+
+
+  loadMap(){
+     let mapContainer = document.getElementById('map');
+      this.map = new google.maps.Map(mapContainer, {
+        center: this.coords,
+        zoom: 12
+      });
+  }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad InicioPage');
